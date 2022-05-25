5a3ac951d26c39097945242338d858e0b78d454c
fullstack <fullstack.mspl@gmail.com>
2022-05-25 18:12:53 +0530

    my init code

---
+++ home_rentals/resources/views/uploadservice_product.blade.php
@@ -0,0 +1,40 @@
+@extends('master.header')
+@section('content')
+<style type="text/css">
+section.aboutsecbanner {
+    background-image: linear-gradient(#05595b, #062C30), url(assets/images/-managed-services-banner162742458088.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    padding: 110px 0 100px;
+    background-position: 90% 43%;
+    background-blend-mode: multiply;
+}
+</style>
+<!-- about banner sec start -->
+<section class="aboutsecbanner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="abouttext2">
+                    <h2>SERVICE FORM</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about banner sec end -->
+
+
+<!-- Service Form start -->
+<section class="service-form">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <h2>PLACE BID ON PROPERTY OR SELECT THE SERVICE ACTIVATE OR DEACTIVATE VIDEO OF RENTAL PROPERTY</h2>
+            </div>
+            
+        </div>
+    </div>
+</section>
+@endsection
